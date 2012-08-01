9af710e8299bc80d157438dd5fd7493e630433fd
hoegertn <thorsten.hoeger@hoegernet.com>
2012-08-01 18:21:48 +0200

    removed old test

--- src/test/java/de/taimos/httputils/Tester1.java
+++ src/test/java/de/taimos/httputils/Tester1.java
@@ -22,20 +22,4 @@ public class Tester1 {
 		Assert.assertFalse(body.isEmpty());
 	}
 
-	/**
-	 * 
-	 */
-	@Test
-	public void testBurnin() {
-		long time = System.nanoTime();
-		for (int i = 0; i < 100; i++) {
-			this.testGet();
-
-			// output system infos like call duration and memory consumption
-			final long duration = ((System.nanoTime() - time) / 1000000);
-			final long mem = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1000000;
-			System.out.println(duration + " ms with ram " + mem);
-			time = System.nanoTime();
-		}
-	}
 }
